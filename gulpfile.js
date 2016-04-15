8861a4f465d3763d4b2e2d265a9d3717653f905b
Jordi Cutillas <jordic@PORTIC.LOCAL>
2016-04-15 15:39:43 +0200

    add responsive

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer'); 
+var autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+var ghPages = require('gulp-gh-pages');
+var validator = require('gulp-html');
+
 
 gulp.task('css', function(){
 	return gulp.src('src/sass/styles.scss')
@@ -12,6 +17,27 @@ gulp.task('css', function(){
             ]
         }))
 		.pipe(gulp.dest('dist/css'));	
+});
+
+gulp.task('image', () => {
+	return gulp.src('src/img/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('deploy', function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('html', function() {
+  return gulp.src('index.html')
+  .pipe(validator())
+  .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('default', ['css'], function(){
